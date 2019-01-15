204baf6408e31c154e233650859ef17713ef3486
Manuel Fernandez <manuel.fernandez@balboadevelopers.com>
2019-01-15 13:37:02 -0500

    change the game's name

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,58 +3,58 @@
 
 const Alexa = require('ask-sdk-core');
 const DECK = [
-  {id: 1, name: 'A', value: 1, suit: 'clubs'},
-  {name: '2', value: 2, suit: 'clubs'},
-  {name: '3', value: 3, suit: 'clubs'},
-  {name: '4', value: 4, suit: 'clubs'},
-  {name: '5', value: 5, suit: 'clubs'},
-  {name: '6', value: 6, suit: 'clubs'},
-  {name: '7', value: 7, suit: 'clubs'},
-  {name: '8', value: 8, suit: 'clubs'},
-  {name: '9', value: 9, suit: 'clubs'},
-  {name: '10', value: 10, suit: 'clubs'},
-  {name: 'J', value: 11, suit: 'clubs'},
-  {name: 'Q', value: 12, suit: 'clubs'},
-  {name: 'K', value: 13, suit: 'clubs'},
-  {name: 'A', value: 1, suit: 'diamonds'},
-  {name: '2', value: 2, suit: 'diamonds'},
-  {name: '3', value: 3, suit: 'diamonds'},
-  {name: '4', value: 4, suit: 'diamonds'},
-  {name: '5', value: 5, suit: 'diamonds'},
-  {name: '6', value: 6, suit: 'diamonds'},
-  {name: '7', value: 7, suit: 'diamonds'},
-  {name: '8', value: 8, suit: 'diamonds'},
-  {name: '9', value: 9, suit: 'diamonds'},
-  {name: '10', value: 10, suit: 'diamonds'},
-  {name: 'J', value: 11, suit: 'diamonds'},
-  {name: 'Q', value: 12, suit: 'diamonds'},
-  {name: 'K', value: 13, suit: 'diamonds'},
-  {name: 'A', value: 1, suit: 'hearts'},
-  {name: '2', value: 2, suit: 'hearts'},
-  {name: '3', value: 3, suit: 'hearts'},
-  {name: '4', value: 4, suit: 'hearts'},
-  {name: '5', value: 5, suit: 'hearts'},
-  {name: '6', value: 6, suit: 'hearts'},
-  {name: '7', value: 7, suit: 'hearts'},
-  {name: '8', value: 8, suit: 'hearts'},
-  {name: '9', value: 9, suit: 'hearts'},
-  {name: '10', value: 10, suit: 'hearts'},
-  {name: 'J', value: 11, suit: 'hearts'},
-  {name: 'Q', value: 12, suit: 'hearts'},
-  {name: 'K', value: 13, suit: 'hearts'},
-  {name: 'A', value: 1, suit: 'spades'},
-  {name: '2', value: 2, suit: 'spades'},
-  {name: '3', value: 3, suit: 'spades'},
-  {name: '4', value: 4, suit: 'spades'},
-  {name: '5', value: 5, suit: 'spades'},
-  {name: '6', value: 6, suit: 'spades'},
-  {name: '7', value: 7, suit: 'spades'},
-  {name: '8', value: 8, suit: 'spades'},
-  {name: '9', value: 9, suit: 'spades'},
-  {name: '10', value: 10, suit: 'spades'},
-  {name: 'J', value: 11, suit: 'spades'},
-  {name: 'Q', value: 12, suit: 'spades'},
-  {name: 'K', value: 13, suit: 'spades'}
+  {name: '2', value: 1, suit: 'clubs'},
+  {name: '3', value: 2, suit: 'clubs'},
+  {name: '4', value: 3, suit: 'clubs'},
+  {name: '5', value: 4, suit: 'clubs'},
+  {name: '6', value: 5, suit: 'clubs'},
+  {name: '7', value: 6, suit: 'clubs'},
+  {name: '8', value: 7, suit: 'clubs'},
+  {name: '9', value: 8, suit: 'clubs'},
+  {name: '10', value: 9, suit: 'clubs'},
+  {name: 'J', value: 10, suit: 'clubs'},
+  {name: 'Q', value: 11, suit: 'clubs'},
+  {name: 'K', value: 12, suit: 'clubs'},
+  {name: 'A', value: 13, suit: 'clubs'},
+  {name: '2', value: 1, suit: 'diamonds'},
+  {name: '3', value: 2, suit: 'diamonds'},
+  {name: '4', value: 3, suit: 'diamonds'},
+  {name: '5', value: 4, suit: 'diamonds'},
+  {name: '6', value: 5, suit: 'diamonds'},
+  {name: '7', value: 6, suit: 'diamonds'},
+  {name: '8', value: 7, suit: 'diamonds'},
+  {name: '9', value: 8, suit: 'diamonds'},
+  {name: '10', value: 9, suit: 'diamonds'},
+  {name: 'J', value: 10, suit: 'diamonds'},
+  {name: 'Q', value: 11, suit: 'diamonds'},
+  {name: 'K', value: 12, suit: 'diamonds'},
+  {name: 'A', value: 13, suit: 'diamonds'},
+  {name: '2', value: 1, suit: 'hearts'},
+  {name: '3', value: 2, suit: 'hearts'},
+  {name: '4', value: 3, suit: 'hearts'},
+  {name: '5', value: 4, suit: 'hearts'},
+  {name: '6', value: 5, suit: 'hearts'},
+  {name: '7', value: 6, suit: 'hearts'},
+  {name: '8', value: 7, suit: 'hearts'},
+  {name: '9', value: 8, suit: 'hearts'},
+  {name: '10', value: 9, suit: 'hearts'},
+  {name: 'J', value: 10, suit: 'hearts'},
+  {name: 'Q', value: 11, suit: 'hearts'},
+  {name: 'K', value: 12, suit: 'hearts'},
+  {name: 'A', value: 13, suit: 'hearts'},
+  {name: '2', value: 1, suit: 'spades'},
+  {name: '3', value: 2, suit: 'spades'},
+  {name: '4', value: 3, suit: 'spades'},
+  {name: '5', value: 4, suit: 'spades'},
+  {name: '6', value: 5, suit: 'spades'},
+  {name: '7', value: 6, suit: 'spades'},
+  {name: '8', value: 7, suit: 'spades'},
+  {name: '9', value: 8, suit: 'spades'},
+  {name: '10', value: 9, suit: 'spades'},
+  {name: 'J', value: 10, suit: 'spades'},
+  {name: 'Q', value: 11, suit: 'spades'},
+  {name: 'K', value: 12, suit: 'spades'},
+  {name: 'A', value: 13, suit: 'spades'},
 ]
 var card1;
 var card2;
@@ -114,9 +114,9 @@ const LaunchRequestHandler = {
     removeCard(card1.name, card1.suit);
     removeCard(card2.name, card2.suit);
 
-    const speechText = `We are going to play a session of in between. I will deal two cards, say yes if
+    const speechText = `We are going to play a session of acey deucey. I will deal two cards, say yes if
                         you believe that the next card I'll deal will be of a value between the two previous cards, or
-                        say dont if you dont believe it.
+                        say no if you dont believe it.
                         Your first cards are: ${card1.name} of ${card1.suit}
                         and ${card2.name} of ${card2.suit}`;
     const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
@@ -233,7 +233,7 @@ const CancelAndStopIntentHandler = {
         || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
   },
   handle(handlerInput) {
-    const speechText = `You got a total of ${points}, thanks for playing`;
+    const speechText = `You got a total of ${points} points, thanks for playing`;
 
     return handlerInput.responseBuilder
       .speak(speechText)
